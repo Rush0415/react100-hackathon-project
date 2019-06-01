c1b759d829df75f89a3dd15ebb698117b0f60834
rush <rushastronomo0415@gmail.com>
2019-06-01 13:14:24 -0700

    final project

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ export default class App extends Component {
     super(props);
     this.state = {
       randomjoke: "",
+      memeLoad: "",
       punch: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.submit = this.submit.bind(this);
     this.getJokes = this.getJokes.bind(this);
+    this.memeImg = this.memeImg.bind(this);
   }
 
   handleChange(e) {
@@ -21,15 +23,26 @@ export default class App extends Component {
 
   submit() {
     this.getJokes();
-
+    this.memeImg();
   }
 
   getJokes() {
-    const reqURL1 = "https://official-joke-api.appspot.com/random_joke";
-    axios.get(reqURL1).then(res => {
+    const reqURL = "https://official-joke-api.appspot.com/random_joke";
+    axios.get(reqURL).then(res => {
       this.setState({
         randomjoke: res.data.setup,
         punch: res.data.punchline
+      });
+    });
+  }
+
+  memeImg() {
+    const reqURL2 = "https://api.memeload.us/v1/random";
+    axios.get(reqURL2).then(res => {
+      // console.log(res.data);
+      this.setState({
+        memeLoad: res.data.image
+        // console.log(image);
       });
     });
   }
@@ -40,7 +53,13 @@ export default class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Hackathon Random App</h1>
           <p className="App-intro">Get Random Jokes and Funny Memes Images</p>
-          <button id="getjoke" className="btn btn-primary" onClick={this.submit}>Click Here</button>
+          <button
+            id="getjoke"
+            className="btn btn-primary"
+            onClick={this.submit}
+          >
+            Click Here
+          </button>
         </header>
 
         <div className="body container-fluid" id="form1">
@@ -49,7 +68,9 @@ export default class App extends Component {
               <div className="row">
                 <div className="col-md-6">
                   <div className="panel panel-default">
-                    <div className="panel-heading" id="header">Random Joke Result</div>
+                    <div className="panel-heading" id="header">
+                      Random Joke Result
+                    </div>
                     <div className="panel-body">
                       <div
                         id="randomjoke"
@@ -64,6 +85,25 @@ export default class App extends Component {
                         onChange={this.handleChange}
                       >
                         {this.state.punch}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="panel panel-default">
+                    <div className="panel-heading" id="header">
+                      RandomMemes Image Result
+                    </div>
+                    <div className="panel-body">
+                      <div
+                        id="joke"
+                        className="container-fluid"
+                        onChange={this.handleChange}
+                      >
+                        <img
+                          src={this.state.memeLoad}className="img-responsive"
+                        />
                       </div>
                     </div>
                   </div>
